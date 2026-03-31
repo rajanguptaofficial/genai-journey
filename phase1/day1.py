7b9b42ac93f677db11aca02c4ee4cd6a6ad7eb20
rajanguptaofficial <rajanguptaofficial@gmail.com>
2026-03-31 10:58:00 +0530

    Day 1 Python basics + calculator program

---
+++ phase1/day1.py
@@ -0,0 +1,12 @@
+print("Hi there!")
+
+name = "Rajan"
+age = "29"
+
+print(f"My name is {name} and I am {age} years old.")
+
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+
+print(f"sum :  {a + b}")
+print(f"multiply: {a*b}")
