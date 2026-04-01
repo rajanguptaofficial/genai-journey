1b21478900031e94ca7d8bb7eee41a7a38827066
rajanguptaofficial <rajanguptaofficial@gmail.com>
2026-04-01 10:24:35 +0530

    day-2 data type, if-else

---
+++ phase1/day2.py
@@ -0,0 +1,11 @@
+age = 5
+weight = 30.4
+name = "Rajan"
+isRocking = True
+
+print(age, weight, name, isRocking)
+
+name = input("enter your name")
+age = int(input("enter your age"))
+
+print(f"hello {name}! you are {age} years old")
---
+++ phase1/day3_if_else.py
@@ -0,0 +1,28 @@
+# age = int(input("enter your age"))
+
+# if age <= 18:
+#     print("underage")
+# else:
+#     print("legal age")   
+
+91
+
+# number = int(input("enter an number"))
+
+# if number%2 == 0:
+#     print("number is even")
+
+# else:
+#     print("number is odd")
+
+
+grade = int(input("enter your grade"))
+
+if grade > 90:
+    print("A")
+elif grade > 75:
+    print("B") 
+elif grade > 50:
+    print("C")
+else:
+    print("Fail")     
