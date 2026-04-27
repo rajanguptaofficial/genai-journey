29dbc65f81ec2b6068a55e1ea315440358f4ab18
rajanguptaofficial <rajanguptaofficial@gmail.com>
2026-04-27 10:36:01 +0530

    phase 4 gen ai apps

--- phase1_python_basics/day7_file_read_write.py
+++ phase1_python_basics/day7_file_read_write.py
@@ -2,4 +2,4 @@ with open("first_file.txt", "w") as file:
     file.write("Hum first")
 
 with open("first_file.txt", "r") as file:
-    print(file.read())  
+    print(file.read())
--- phase2_api_json/day10_work_with_json.py
+++ phase2_api_json/day10_work_with_json.py
@@ -5,6 +5,5 @@ response = requests.get("https://api.github.com/users")
 data = response.json()
 
 print(data[0]["login"])
-
 print(data[0]["node_id"])
 print(data[0]["avatar_url"])
